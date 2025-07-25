d38c80a0c300d2739cc60718a1cf3db97ca39800
shadow01a <shadow01a@users.noreply.github.com>
2025-07-25 14:32:29 +0000

    更新Linux文档，添加模型下载

--- assets/chunks/channel.BX0jsasD.js
+++
@@ -1,1 +0,0 @@
-import{U as a,R as n}from"../app.BBecp2KB.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ assets/chunks/channel.DPHdWEDO.js
@@ -0,0 +1,1 @@
+import{U as a,R as n}from"../app.Cay5XbdP.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
